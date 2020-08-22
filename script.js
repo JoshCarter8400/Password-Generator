2b6f35af035ff9da288545b7d2643face399e706
Josh Carter <joshcarter8400@gmail.com>
2020-08-21 21:34:45 -0400

    spelling errors corrected

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var generateBtn = document.querySelector("#generate");
-
-
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ];
@@ -11,7 +8,7 @@ var symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", "/", ";", 
 // }
 
 // Get references to the #generate element
-
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -26,7 +23,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-    var passLength = prompt("Please choose a password that is at least 8 character long and no more than 128.");
+    var passLength = prompt("Please choose a length of your password that is at least 8 character long and no more than 128.");
     var passUpper = confirm("Would you Like uppercase letters.");
     var passLower = confirm("Would you Like lowercase letters.");
     var passNumber = confirm("Would you Like numbers.");
