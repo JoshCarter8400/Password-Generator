a6cc24844d347749142e0c8802522540226ac7b4
Josh Carter <joshcarter8400@gmail.com>
2020-08-22 14:01:19 -0400

    edited code to take out some comments

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ];
 var symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", "/", ";", "@", "?", "]", "|", "`"];
 var char = []
-    // for (i = 0; i < upperCase.length; i++) {
-    //     console.log(upperCase[i]);
-    // }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
