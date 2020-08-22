79bb1f62861e514f0490f9d4ee308f2371980003
Josh Carter <joshcarter8400@gmail.com>
2020-08-22 13:57:56 -0400

    added password return capability

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ];
 var symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", "/", ";", "@", "?", "]", "|", "`"];
-
-// for (i = 0; i < upperCase.length; i++) {
-//     console.log(upperCase[i]);
-// }
+var char = []
+    // for (i = 0; i < upperCase.length; i++) {
+    //     console.log(upperCase[i]);
+    // }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
@@ -23,7 +25,15 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-    var passLength = prompt("Please choose a length of your password that is at least 8 character long and no more than 128.");
+    var passLength = parseInt(prompt("Please choose a length of your password that is at least 8 character long and no more than 128."));
+    if (passLength < 8) {
+        alert("Please choose a password longer than 8 Characters");
+        return;
+    }
+    if (passLength > 128) {
+        alert("Please choose a password less than 128 Characters");
+        return;
+    }
     var passUpper = confirm("Would you Like uppercase letters.");
     var passLower = confirm("Would you Like lowercase letters.");
     var passNumber = confirm("Would you Like numbers.");
@@ -50,7 +60,11 @@ function generatePassword() {
         var randomNum = Math.floor(Math.random() * userChoices.length);
         var character = userChoices[randomNum];
         console.log(character);
+
+        char.push(character);
+
     }
 
-    return ("password");
+    return char.join("");
+
 }
